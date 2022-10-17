cb5cafc91375bd2c4f6238f014708ef3b7001e85
李岚清 <958614130@qq.com>
2022-10-17 12:14:05 +0800

    feat: 初始化rc-use-hooks库

--- config/config.ts
+++ config/config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'dumi';
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
 export default defineConfig({
-  publicPath: (process.env.NODE = 'development' ? '/' : '/lighting-design/docs-dist/'),
-  base: (process.env.NODE = 'development' ? '/' : '/lighting-design/docs-dist/'),
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/lighting-design/docs-dist/',
+  base: process.env.NODE_ENV === 'development' ? '/' : '/lighting-design/docs-dist/',
   title: 'l-design',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
