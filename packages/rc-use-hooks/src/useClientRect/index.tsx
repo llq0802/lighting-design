9895b41977af6241427bacc1ce697c75690fe466
李岚清 <958614130@qq.com>
2022-10-24 16:33:02 +0800

    feat: 新增useClientRect

---
+++ packages/rc-use-hooks/src/useClientRect/index.tsx
@@ -0,0 +1,31 @@
+// ref:
+//  https://zh-hans.reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
+import { useEffect, useRef, useState } from 'react';
+
+/**
+ * 测量 DOM 节点 整个重新渲染期间始终存在
+ */
+function useClientRect() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [rect, setRect] = useState<DOMRect>();
+  useEffect(() => {
+    if (ref.current) {
+      setRect(ref.current?.getBoundingClientRect());
+    }
+  }, [ref]);
+
+  return [rect, ref] as const;
+}
+
+//  官方案例 当且仅当组件挂载和卸载时
+// function useClientRect() {
+//   const [rect, setRect] = useState(null);
+//   const ref = useCallback(node => {
+//     if (node !== null) {
+//       setRect(node.getBoundingClientRect());
+//     }
+//   }, []);
+//   return [rect, ref];
+// }
+
+export default useClientRect;
---
+++ packages/rc-use-hooks/src/usePersistFn/index.ts
@@ -0,0 +1,38 @@
+import { useCallback, useRef } from 'react';
+
+/**
+ * 持久函数,地址永远不会改变
+ * @param fn
+ * @returns
+ */
+function usePersistFn<T extends (...args: any[]) => any>(fn: T): T extends infer R ? R : T {
+  const ref = useRef<T>(fn);
+  ref.current = fn;
+
+  const persistFn = useCallback(
+    (...args: any[]) => {
+      const refFn = ref.current;
+      return refFn?.(...args);
+    },
+    [ref],
+  );
+
+  return persistFn as any;
+}
+
+export default usePersistFn;
+
+//  https://zh-hans.reactjs.org/docs/hooks-faq.html#how-to-read-an-often-changing-value-from-usecallback
+
+// function useEventCallback(fn, dependencies) {
+//   const ref = useRef(() => {
+//     throw new Error('Cannot call an event handler while rendering.');
+//   });
+//   useEffect(() => {
+//     ref.current = fn;
+//   }, [fn, ...dependencies]);
+//   return useCallback(() => {
+//     const fn = ref.current;
+//     return fn();
+//   }, [ref]);
+// }
