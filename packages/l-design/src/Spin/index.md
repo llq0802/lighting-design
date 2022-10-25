e56b4c6fd6c4113c7ff28e7a40582c78d8ec2b13
Yin <1940637041@qq.com>
2022-10-25 16:45:59 +0800

    fix: spin修改

--- packages/l-design/src/Spin/index.md
+++ packages/l-design/src/Spin/index.md
@@ -6,9 +6,20 @@ nav:
   path: /components
 ---
 
-```
+# Spin 加载
 
+用于页面和区块组件的加载状态。
 
-```
+## 基本用法
 
-<code src="./demos/base.tsx"></code>
+<code src="./demos/base.tsx"/>
+
+## 改变大小与标识符
+
+<code src="./demos/indicator-size.tsx"/>
+
+## 放入容器动态改变 loading
+
+<code src="./demos/inside.tsx"/>
+
+<API></API>
--- packages/l-design/src/Spin/index.tsx
+++ packages/l-design/src/Spin/index.tsx
@@ -1,22 +1,49 @@
 /* eslint-disable react/jsx-key */
+import classNames from 'classnames';
 import type { FC } from 'react';
 import Loading1 from './component/loading1';
 import Loading2 from './component/loading2';
 import Loading3 from './component/loading3';
+import './index.less';
 
 interface LoadingProps {
-  width: number;
-  height: number;
+  /**
+   * 图标类型
+   */
   mode: 'load1' | 'load2' | 'load3';
+  /**
+   * 图标颜色
+   */
   color: string;
+  /**
+   * 组件大小，可选值为 small default large
+   */
   size: 'small' | 'default' | 'large';
+  /**
+   * 自定义描述文案
+   */
   tip: string;
+  /**
+   * 描述文案颜色
+   */
+  tipColor: string;
+  /**
+   * 加载状态
+   */
+  loading: boolean;
+  /**
+   * ReactNode
+   */
+  children: React.ReactNode;
 }
-const Loading: FC<Partial<LoadingProps>> = ({
+const Index: FC<Partial<LoadingProps>> = ({
   color = '#FF6700',
   size = 'default',
   mode = 'load1',
   tip,
+  loading,
+  tipColor = '#666',
+  children,
 }) => {
   const props = { fill: color, size };
   const loadArr = [
@@ -27,7 +54,9 @@ const Loading: FC<Partial<LoadingProps>> = ({
     },
   ];
 
-  return (
+  const isNestedPattern = () => typeof children !== 'undefined';
+
+  const spinElement = (
     <div
       style={{
         display: 'flex',
@@ -36,11 +65,28 @@ const Loading: FC<Partial<LoadingProps>> = ({
       }}
     >
       {loadArr.map((item) => item[mode])}
-      <span>
+      <span className="y-spin-tip" style={{ color: tipColor }}>
         {tip}
       </span>
     </div>
   );
+
+  if (isNestedPattern()) {
+    return (
+      <div className="y-spin-content" style={{ position: 'relative' }}>
+        {loading && <div className="y-spin-loading">{spinElement}</div>}
+        <div
+          className={classNames(`y-spin-container`, {
+            [`y-spin--blur`]: loading,
+          })}
+        >
+          {children}
+        </div>
+      </div>
+    );
+  }
+
+  return spinElement;
 };
 
-export default Loading;
+export default Index;
