813fa6570adb82b0e02d82e40b96f3da5a203a46
李岚清 <958614130@qq.com>
2023-04-28 09:58:14 +0800

    docs: 更新文档

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist' },
+  cjs: {
+    output: 'dist/lib',
+  },
+  esm: {
+    output: 'dist/es',
+  },
 });
