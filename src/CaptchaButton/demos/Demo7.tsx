70e4bede38167828e6f12d589db306309f5bad33
李岚清 <958614130@qq.com>
2023-11-25 17:17:52 +0800

    fix: 优化 Form 代码

--- src/CaptchaButton/demos/Demo7.tsx
+++ src/CaptchaButton/demos/Demo7.tsx
@@ -1,22 +1,23 @@
+import { Button } from 'antd';
 import { useEffect, useState } from 'react';
 
-const Demo7 = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}> Demo7-{count}</button>
-      <hr />
-      <Child1 key={count}></Child1>
-    </div>
-  );
-};
-
-function Child1(params: type) {
+function Child1() {
   useEffect(() => {
     console.log('===Child1===>');
   }, []);
 
   return <div>Child1</div>;
 }
+
+const Demo7 = () => {
+  const [count, setCount] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setCount(!count)}> Demo7-{JSON.stringify(count)}</Button>
+      <hr />
+      <Child1 key={`${count}`}></Child1>
+    </div>
+  );
+};
 
 export default Demo7;
