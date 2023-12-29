f196a35dc0e8653da6823587168b93be4c3978a7
李岚清 <958614130@qq.com>
2023-12-29 11:10:58 +0800

    fix: fix修复 LSliderVerify 响应式

--- src/QueryForm/demos/Demo6.tsx
+++ src/QueryForm/demos/Demo6.tsx
@@ -1,3 +1,4 @@
+import { Button, Space } from 'antd';
 import { LForm, LFormItemInput, LQueryForm } from 'lighting-design';
 
 const Demo1 = () => {
@@ -11,8 +12,18 @@ const Demo1 = () => {
         onFinish={async (values) => {
           console.log('onFinish-values ', values);
         }}
+        submitter={{
+          render(dom) {
+            return (
+              <Space>
+                <Button type="primary">新增</Button>
+                {dom}
+              </Space>
+            );
+          },
+        }}
       >
-        <LFormItemInput name="inpu1" required label="输入框1" />
+        <LFormItemInput name="input1" required label="输入框1" />
         <LFormItemInput name="input2" required label="输入框2" />
       </LQueryForm>
     </>
