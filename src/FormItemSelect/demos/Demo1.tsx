d34da419c27f520ce5ed5ff72feebc8f164e9f51
李岚清 <958614130@qq.com>
2024-02-07 16:22:11 +0800

    docs: 📝更新文档实例

--- src/FormItemSelect/demos/Demo1.tsx
+++ src/FormItemSelect/demos/Demo1.tsx
@@ -4,27 +4,42 @@ const Demo1 = () => {
   const [form] = LForm.useForm();
 
   return (
-    <LForm form={form} labelWidth={90} submitter={{ buttonAlign: 'center' }}>
+    <LForm form={form} labelWidth={180} submitter={{ buttonAlign: 'center' }}>
       <LFormItemSelect
-        label="select1"
+        label="单选"
         name="select1"
         required
         options={[
-          { label: 'Unresolved', value: 'open' },
-          { label: 'Resolved', value: 'closed' },
-          { label: 'Resolving', value: 'processing' },
+          { label: 'open', value: 'open' },
+          { label: 'closed', value: 'closed' },
+          { label: 'processing', value: 'processing' },
         ]}
+        all
       />
       <LFormItemSelect
-        label="select2"
+        label="多选"
         name="select2"
         required
         all
         options={[
-          { label: 'Unresolved', value: 'open' },
-          { label: 'Resolved', value: 'closed' },
-          { label: 'Resolving', value: 'processing' },
+          { label: 'Unresolved', value: 'Unresolved' },
+          { label: 'Resolved', value: 'Resolved' },
+          { label: 'Resolving', value: 'Resolving' },
         ]}
+        selectProps={{ mode: 'multiple' }}
+      />
+      <LFormItemSelect
+        label="自定义 options 的键名"
+        name="select3"
+        required
+        all
+        allValue=""
+        options={[
+          { name: 'open', id: 0 },
+          { name: 'closed', id: 'closed' },
+          { name: 'processing', id: 'processing' },
+        ]}
+        selectProps={{ fieldNames: { label: 'name', value: 'id' } }}
       />
     </LForm>
   );
--- src/FormItemSelect/index.md
+++ src/FormItemSelect/index.md
@@ -35,7 +35,9 @@ nav:
 
 :::warning{title=注意}
 
-- `request`与`options`同时配置时 `options`优先级更高
+- 数据优先级: `组件本身 options` > `selectProps.options` > `request 返回值`
+
+- `all`只在单选生效, 如果想在多选也有`全部`选项 , 需要配置`selectProps.dropdownRender`自行执行处理
 
 - `request` 的参数集合`depends` 为 `LFormItem` 组件 `dependencies` 属性依赖项字段的值 , 如果依赖项发生变化则会自动请求数据 , 如果依赖项的值为`假值或者空数组`则本身数据也会清除或者禁用选择
 
@@ -53,7 +55,7 @@ import { LFormItemSelect } from 'lighting-design';
 | request        | 异步请求数据函数                        | `(...depends: any[]) => Promise<LSelectOptions[]>`          | `-`     |
 | requestOptions | ahook 的 request 的配置项               | `-`                                                         | `-`     |
 | debounceTime   | 当依赖项发生变化时重新请求的防抖时间    | `number`                                                    | `-`     |
-| all            | 是否需要全部选项                        | `boolean `                                                  | `false` |
+| all            | 是否需要全部选项 `(只再单选模式下生效)` | `boolean `                                                  | `false` |
 | allLabel       | 全部选项的名称                          | `string `                                                   | `全部`  |
 | allValue       | 全部选项的值                            | `string`                                                    | `'all'` |
 | spin           | 配置`request`时自定义`loading`效果      | [SpinProps](https://ant.design/components/spin-cn/#api)     | `-`     |
