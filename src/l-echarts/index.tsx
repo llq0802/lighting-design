7e7b19a5086175b6657da37ba6e2f330652d620f
李岚清 <958614130@qq.com>
2025-08-19 09:14:36 +0800

    - 移除了未使用的 useMemoizedFn 导入 - 优化了多个函数的定义，去除了不必要的 useMemoizedFn 包装 - 简化了某些函数的实现，提高了代码可读性 - 修复了一些潜在的类型相关问题

--- src/l-echarts/index.tsx
+++ src/l-echarts/index.tsx
@@ -1,10 +1,10 @@
-import { useMemoizedFn, useMount, usePrevious, useUnmount, useUpdateEffect } from 'ahooks';
+import { useMount, usePrevious, useUnmount, useUpdateEffect } from 'ahooks';
 import classnames from 'classnames';
 import type { ECharts, EChartsType } from 'echarts';
 import * as echarts from 'echarts';
 import isEqual from 'fast-deep-equal';
 import { emptyArray, emptyObject } from 'lighting-design/constants';
-import { fastDeepClone, isFunction, transformEchartsOption } from 'lighting-design/utils';
+import { fastDeepClone, transformEchartsOption } from 'lighting-design/utils';
 import { isString, pick } from 'lodash-es';
 import { memo, useImperativeHandle, useRef, type FC } from 'react';
 import { bind, clear } from 'size-sensor';
@@ -35,22 +35,22 @@ const LECharts: FC<LEChartsProps> = memo((props) => {
     designWidth = 1920,
     autoResizeFields,
   } = props;
-  const ref = useRef<HTMLDivElement | null>(null);
+  const ref = useRef<HTMLDivElement>(null!);
   const echartsInstanceRef = useRef<ECharts>();
   const isInitialResize = useRef(true);
   const unBind = useRef<() => void>();
   const prevProps = usePrevious(props);
 
   /** echarts.init 初始化实例 */
-  const initEchartsInstance = useMemoizedFn(async () => {
+  const initEchartsInstance = async () => {
     return new Promise((resolve) => {
-      const echartsInstance = (echartsInstanceRef.current = echarts.init(ref.current, theme, opts));
-      resolve(echartsInstance);
+      const ins = (echartsInstanceRef.current = echarts.init(ref.current, theme, opts));
+      resolve(ins);
     });
-  });
+  };
 
   /** 更新图表 */
-  const updateEChartsOption = useMemoizedFn(() => {
+  const updateEChartsOption = () => {
     let echartOption;
     if (autoResize && autoResizeFields !== false && (autoResizeFields?.length || autoResizeFields === void 0)) {
       echartOption = transformEchartsOption(
@@ -59,19 +59,19 @@ const LECharts: FC<LEChartsProps> = memo((props) => {
         designWidth,
       );
     } else {
-      echartOption = option;
+      echartOption = fastDeepClone(option);
     }
 
     const echartInstance = echartsInstanceRef.current;
     echartInstance?.setOption(echartOption, notMerge, lazyUpdate);
     if (showLoading) echartInstance?.showLoading(loadingOption);
     else echartInstance?.hideLoading();
-  });
+  };
 
   /** 绑定图表实例事件 */
-  const bindEvents = useMemoizedFn((instance: ECharts, events: Record<string, (params: any, ins: ECharts) => void>) => {
+  const bindEvents = (instance: ECharts, events: Record<string, (params: any, ins: ECharts) => void>) => {
     function _bindEvent(eventName: string, func: (params: any, ins: ECharts) => void) {
-      if (isString(eventName) && isFunction(func)) {
+      if (isString(eventName) && typeof func === 'function') {
         instance.on(eventName, (param: any) => {
           func(param, instance);
         });
@@ -81,9 +81,11 @@ const LECharts: FC<LEChartsProps> = memo((props) => {
     Object.keys(events)?.forEach((eventName) => {
       _bindEvent(eventName, events[eventName]);
     });
-  });
+  };
   /** 创建图表 */
-  const renderNewEcharts = useMemoizedFn(async () => {
+  const renderNewEcharts = async () => {
+    if (!ref.current) return;
+
     await initEchartsInstance();
 
     updateEChartsOption();
@@ -92,21 +94,18 @@ const LECharts: FC<LEChartsProps> = memo((props) => {
 
     bindEvents(echartInstance, onEvents || {});
 
-    if (isFunction(onChartReady)) onChartReady?.(echartInstance);
-
-    if (!ref.current) return;
+    if (typeof onChartReady === 'function') onChartReady?.(echartInstance);
 
     unBind.current = bind(ref.current, (dom) => {
       // 解决闪动问题
       if (dom!.clientWidth <= 0 || dom!.clientHeight <= 0) return;
-      // eslint-disable-next-line @typescript-eslint/no-use-before-define
       if (autoResize) resize();
       onChartResize?.(echartInstance, dom as HTMLDivElement);
     });
-  });
+  };
 
   /** 销毁当前图表实例并取消dom宽高监听 */
-  const dispose = useMemoizedFn(() => {
+  const dispose = () => {
     unBind.current?.();
     echartsInstanceRef.current?.clear?.();
     echartsInstanceRef.current?.dispose?.();
@@ -118,10 +117,10 @@ const LECharts: FC<LEChartsProps> = memo((props) => {
       }
       echarts?.dispose(ref.current);
     }
-  });
+  };
 
   /** 调用图标实例 resize函数 并加动画 */
-  const resize = useMemoizedFn(() => {
+  const resize = () => {
     const echartsInstance = echartsInstanceRef.current;
     if (!isInitialResize.current) {
       try {
@@ -139,13 +138,13 @@ const LECharts: FC<LEChartsProps> = memo((props) => {
       }
     }
     isInitialResize.current = false;
-  });
+  };
 
   useUpdateEffect(() => {
     // 第二步优化性能
 
     // 如果返回 false 直接不更新
-    if (isFunction(shouldSetOption) && !shouldSetOption?.(prevProps, props)) {
+    if (typeof shouldSetOption === 'function' && !shouldSetOption?.(prevProps, props)) {
       return;
     }
 
